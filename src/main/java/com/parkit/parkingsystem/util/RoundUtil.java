fefc9ef665fabc7c342ee3694b2eecd6b62c6cdb
Monsieur 486 <monsieur486@gmail.com>
2023-07-11 13:30:41 +0200

    Pris en compte de la réduction pour uasgers récurents

---
+++ src/main/java/com/parkit/parkingsystem/util/RoundUtil.java
@@ -0,0 +1,7 @@
+package com.parkit.parkingsystem.util;
+
+public class RoundUtil {
+    public static double threeDigitRender(double number){
+        return Math.round(number * 1000.0) / 1000.0;
+    }
+}
