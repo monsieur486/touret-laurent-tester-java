da49e0c5adb8903cd51741dc53e1466c5a9030ad
Monsieur 486 <monsieur486@gmail.com>
2023-07-13 16:41:15 +0200

    Factorisation Roundutil tests

--- src/main/java/com/parkit/parkingsystem/util/RoundUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/RoundUtil.java
@@ -1,6 +1,8 @@
 package com.parkit.parkingsystem.util;
 
 public class RoundUtil {
+
+    private RoundUtil(){}
     public static double threeDigitRender(double number) {
         return Math.round(number * 1000.0) / 1000.0;
     }
---
+++ src/test/java/com/parkit/parkingsystem/util/RoundUtilTest.java
@@ -0,0 +1,18 @@
+package com.parkit.parkingsystem.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoundUtilTest {
+
+    @Test
+    void threeDigitRenderLow() {
+        assertEquals(0.333,RoundUtil.threeDigitRender (0.33333333333));
+    }
+
+    @Test
+    void threeDigitRenderHigh() {
+        assertEquals(1.0,RoundUtil.threeDigitRender (0.9999999999));
+    }
+}
